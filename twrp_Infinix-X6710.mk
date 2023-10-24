a0d83a457900d488a510e3d6b162e035f50ac121
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-10-24 19:21:44 +0300

    Needed

--- twrp_Infinix-X6710.mk
+++ twrp_Infinix-X6710.mk
@@ -10,20 +10,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omn/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from Infinix-X6710 device
 $(call inherit-product, device/infinix/Infinix-X6710/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6710
-PRODUCT_NAME := omni_Infinix-X6710
+PRODUCT_NAME := twrp_Infinix-X6710
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6710
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6710_h931-user 12 SP1A.210812.016 421093 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6710-GL/Infinix-X6710:12/SP1A.210812.016/230904V1822:user/release-keys
